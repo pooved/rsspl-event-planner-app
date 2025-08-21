3282cf05cc10e9496ae6568500466c0b7ebf650d
pooved <poo112.naik@gmail.com>
2025-08-21 13:25:56 +0530

    Restucturing code

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,21 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
-import { AuthContext } from "../store/AuthContext";
-import { useContext } from "preact/hooks";
 
 export default function NavLinks() {
-     const context= useContext(AuthContext);
-      if (!context) {
-      throw new Error("Error");
-    }
-     const {state}=context;
-     const {isAuthenticated}=state
   return (
     <div className="text-sm lg:flex lg:flex-grow">
-       {isAuthenticated ? (
-        <>
-        <p className="text-white lg:mr-2 mr-0 mt-2 lg:mt-0">Welcome!, {state.user?.userName}</p>
-          <NavLink
+      <NavLink
         to="/create"
         className="block mt-4 px-2  lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 cursor-pointer"
       >
@@ -27,9 +16,6 @@ export default function NavLinks() {
       >
         Past Events
       </NavLink>
-         </>
-      ) : ""}
-    
       <ThemeToggle />
     </div>
   );
